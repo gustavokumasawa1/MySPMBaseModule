9410f9b8061d4227ed2630e143d739eed448e9c8
gustavokumasawa1 <gustavo.kumasawa@provider-it.com.br>
2024-09-25 12:20:45 -0300

    RandomColorGenerator

---
+++ Sources/MySPMBaseModule/RandomColorGenerator.swift
@@ -0,0 +1,14 @@
+import UIKit
+
+public class RandomColorGenerator {
+    
+    public static func get() -> UIColor {
+        return UIColor(
+            red: .random(in: 0...1),
+            green: .random(in: 0...1),
+            blue: .random(in: 0...1),
+            alpha: 1
+        )
+    }
+    
+}
